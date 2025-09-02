3befa11cd8fa2eba86543c85f7f6c1c0fc65e77a
Trenchy <128204689+Trenchfoote@users.noreply.github.com>
2025-09-02 16:28:58 -0500

    Rename MyPlugin to TrenchyUI throughout addon
    Replaces all references to MyPlugin with TrenchyUI, updates default options, function names, and config registration to reflect the new module name. This improves clarity and aligns the code with the intended addon branding.

--- ElvUI-TrenchyUI/ElvUI_TrenchyUI.lua
+++ ElvUI-TrenchyUI/ElvUI_TrenchyUI.lua
@@ -5,21 +5,21 @@
 ]]
 
 local E, L, V, P, G = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
-local MyPlugin = E:NewModule('MyPluginName', 'AceHook-3.0', 'AceEvent-3.0', 'AceTimer-3.0'); --Create a plugin within ElvUI and adopt AceHook-3.0, AceEvent-3.0 and AceTimer-3.0. We can make use of these later.
+local TrenchyUI = E:NewModule('TrenchyUI', 'AceHook-3.0', 'AceEvent-3.0', 'AceTimer-3.0'); --Create a plugin within ElvUI and adopt AceHook-3.0, AceEvent-3.0 and AceTimer-3.0. We can make use of these later.
 local EP = LibStub("LibElvUIPlugin-1.0") --We can use this to automatically insert our GUI tables when ElvUI_Config is loaded.
 local addonName, addonTable = ... --See http://www.wowinterface.com/forums/showthread.php?t=51502&p=304704&postcount=2
 
 --Default options
-P["MyPlugin"] = {
+P["TrenchyUI"] = {
 	["SomeToggleOption"] = true,
 	["SomeRangeOption"] = 5,
 }
 
 --Function we can call when a setting changes.
 --In this case it just checks if "SomeToggleOption" is enabled. If it is it prints the value of "SomeRangeOption", otherwise it tells you that "SomeToggleOption" is disabled.
-function MyPlugin:Update()
-	local enabled = E.db.MyPlugin.SomeToggleOption
-	local range = E.db.MyPlugin.SomeRangeOption
+function TrenchyUI:Update()
+	local enabled = E.db.TrenchyUI.SomeToggleOption
+	local range = E.db.TrenchyUI.SomeRangeOption
 
 	if enabled then
 		print(range)
@@ -29,22 +29,22 @@ function MyPlugin:Update()
 end
 
 --This function inserts our GUI table into the ElvUI Config. You can read about AceConfig here: http://www.wowace.com/addons/ace3/pages/ace-config-3-0-options-tables/
-function MyPlugin:InsertOptions()
-	E.Options.args.MyPlugin = {
+function TrenchyUI:InsertOptions()
+	E.Options.args.TrenchyUI = {
 		order = 100,
 		type = "group",
-		name = "MyPlugin",
+		name = "|cffff2f3dTrenchyUI|r",
 		args = {
 			SomeToggleOption = {
 				order = 1,
 				type = "toggle",
 				name = "MyToggle",
 				get = function(info)
-					return E.db.MyPlugin.SomeToggleOption
+					return E.db.TrenchyUI.SomeToggleOption
 				end,
 				set = function(info, value)
-					E.db.MyPlugin.SomeToggleOption = value
-					MyPlugin:Update() --We changed a setting, call our Update function
+					E.db.TrenchyUI.SomeToggleOption = value
+					TrenchyUI:Update() --We changed a setting, call our Update function
 				end,
 			},
 			SomeRangeOption = {
@@ -55,20 +55,20 @@ function MyPlugin:InsertOptions()
 				max = 10,
 				step = 1,
 				get = function(info)
-					return E.db.MyPlugin.SomeRangeOption
+					return E.db.TrenchyUI.SomeRangeOption
 				end,
 				set = function(info, value)
-					E.db.MyPlugin.SomeRangeOption = value
-					MyPlugin:Update() --We changed a setting, call our Update function
+					E.db.TrenchyUI.SomeRangeOption = value
+					TrenchyUI:Update() --We changed a setting, call our Update function
 				end,
 			},
 		},
 	}
 end
 
-function MyPlugin:Initialize()
+function TrenchyUI:Initialize()
 	--Register plugin so options are properly inserted when config is loaded
-	EP:RegisterPlugin(addonName, MyPlugin.InsertOptions)
+	EP:RegisterPlugin(addonName, TrenchyUI.InsertOptions)
 end
 
-E:RegisterModule(MyPlugin:GetName()) --Register the module with ElvUI. ElvUI will now call MyPlugin:Initialize() when ElvUI is ready to load our plugin.
+E:RegisterModule(TrenchyUI:GetName()) --Register the module with ElvUI. ElvUI will now call MyPlugin:Initialize() when ElvUI is ready to load our plugin.
