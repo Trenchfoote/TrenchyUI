27a60e22a373acbf92205fa897b64f055da35de9
Eltreum <30246110+eltreum0@users.noreply.github.com>
2025-09-06 03:20:47 -0300

    rename variables, use standard elvui import

--- ElvUI_TrenchyUI/Settings/Profile.lua
+++ ElvUI_TrenchyUI/Settings/Profile.lua
@@ -2,15 +2,22 @@ local E, _, _, P = unpack(ElvUI)
 
 -- Trenchy UI Profile DB
 P.ElvUI_TrenchyUI = {
-	StyleFiltersSeasonal = {
-		Expansion = "TWW",
-		Season = 3,
-		Version = 20250905,
-	},
-	StyleFiltersCommon = {
-		Expansion = "TWW",
-		Season = 3,
-		Version = 20250906,
+	StyleFilters = {
+		Installed = {
+			type = "",
+			Season = 0,
+			Version = 0,
+		},
+		Seasonal = {
+			Expansion = "TWW",
+			Season = 3,
+			Version = 20250905,
+		},
+		Common = {
+			Expansion = "TWW",
+			Season = 3,
+			Version = 20250906,
+		},
 	},
 	install_version = nil,
 	warpdeplete = {
