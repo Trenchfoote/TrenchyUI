536bd05cca3919d97d4771879442cc92893550de
Eltreum <30246110+eltreum0@users.noreply.github.com>
2025-09-06 17:43:49 -0300

    Revert "Texture refresh"
    This reverts commit 28c0d205e675da9124dc6a36fb20d23a2a09cc75.

--- ElvUI_TrenchyUI/Media/SharedMedia.lua
+++ ElvUI_TrenchyUI/Media/SharedMedia.lua
@@ -3,6 +3,7 @@ local LSM = E.Libs.LSM
 
 -- Statusbar Textures
 LSM:Register('statusbar', 'TrenchyBlank', [[Interface\AddOns\ElvUI_TrenchyUI\Media\statusbar\TrenchyBlank.tga]])
+LSM:Register('statusbar', 'TrenchyChevron', [[Interface\AddOns\ElvUI_TrenchyUI\Media\statusbar\TrenchyChevron.tga]])
 LSM:Register('statusbar', 'TrenchyFocus', [[Interface\AddOns\ElvUI_TrenchyUI\Media\statusbar\TrenchyFocus.tga]])
 LSM:Register('statusbar', 'TrenchyFocus2', [[Interface\AddOns\ElvUI_TrenchyUI\Media\statusbar\TrenchyFocus2.tga]])
 LSM:Register('statusbar', 'TrenchyKick', [[Interface\AddOns\ElvUI_TrenchyUI\Media\statusbar\TrenchyKick.tga]])
